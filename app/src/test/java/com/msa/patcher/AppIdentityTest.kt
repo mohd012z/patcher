2eafde1497c2c8b3d1b0770a9dc5c153b5c8c0c2
mohd012z <msafingulanam@gmail.com>
2026-08-31 08:47:34 +0800

    Fix V8.1 version unit test

--- app/src/test/java/com/msa/patcher/AppIdentityTest.kt
+++ app/src/test/java/com/msa/patcher/AppIdentityTest.kt
@@ -1,6 +1,11 @@
 package com.msa.patcher
+
 import org.junit.Assert.assertEquals
 import org.junit.Test
+
 class AppIdentityTest {
- @Test fun versionIsV8() { assertEquals("8.0", BuildConfig.VERSION_NAME) }
+    @Test
+    fun versionIsV81() {
+        assertEquals("8.1", BuildConfig.VERSION_NAME)
+    }
 }
